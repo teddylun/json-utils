63087c86b181a1426b664ea547384b986be516b6
teddyhlun <teddyhlun@gmail.com>
2020-09-01 00:29:40 +0700

    escape & unescape

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { window, ExtensionContext, commands } from "vscode";
+import {
+  window,
+  ExtensionContext,
+  commands,
+  TextEditor,
+  Position,
+  Range,
+  Selection,
+} from "vscode";
 import Helper from "./helper";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,6 +21,40 @@ export function activate(context: ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
+  /**
+   * This function is used to set the current document text
+   * @param newText
+   */
+  let setText = (editor: TextEditor, newText: string) => {
+    let doc = editor.document;
+    editor.edit((builder) => {
+      let start, end;
+
+      // Format whole file or selected text
+      if (editor.selection.isEmpty) {
+        const lastLine = doc.lineAt(doc.lineCount - 1);
+        start = new Position(0, 0);
+        end = new Position(doc.lineCount - 1, lastLine.text.length);
+      } else {
+        start = editor.selection.start;
+        end = editor.selection.end;
+      }
+
+      // replace text
+      builder.replace(new Range(start, end), newText);
+
+      // Select the whole json
+      editor.selection = new Selection(start, end);
+    });
+  };
+
+  let removeWhiteSpace = (text: string): string => {
+    return text.trim().replace(/(?:^[\n\t\r]|[\n\t\r]$)/g, "");
+  };
+
+  /**
+   * validateJson
+   */
   let validateJson = commands.registerCommand("extension.validateJson", () => {
     let editor = window.activeTextEditor;
     if (!editor) {
@@ -23,13 +65,64 @@ export function activate(context: ExtensionContext) {
     let text = doc.getText(editor.selection) || doc.getText();
 
     // Remove trailing and leading whitespace
-    let trimmedText = text.trim().replace(/(?:^[\n\t\r]|[\n\t\r]$)/g, "");
+    let trimmedText = removeWhiteSpace(text);
     helper.isValid(trimmedText)
       ? window.showInformationMessage("Valid JSON")
       : window.showErrorMessage("Invalid JSON");
   });
 
+  /**
+   * escapeJson
+   */
+  let escapeJson = commands.registerCommand("extension.escapeJson", () => {
+    // Get active editor
+    let editor = window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    // Get the document
+    let doc = editor.document;
+    let text = doc.getText(editor.selection) || doc.getText();
+
+    // Remove trailing and leading whitespace
+    let trimmedText = removeWhiteSpace(text);
+
+    // Escape JSON
+    let escapedJson = helper.escape(trimmedText);
+    if (escapedJson !== trimmedText) {
+      setText(editor, escapedJson);
+    }
+  });
+
+  /**
+   * unescapeJson
+   */
+  let unescapeJson = commands.registerCommand("extension.unescapeJson", () => {
+    // Get active editor
+    let editor = window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    // Get the document
+    let doc = editor.document;
+    let text = doc.getText(editor.selection) || doc.getText();
+
+    // Remove trailing and leading whitespace
+    let trimmedText = removeWhiteSpace(text);
+
+    // Unescape JSON
+    let unescapedJson = helper.unescape(trimmedText);
+
+    if (unescapedJson !== trimmedText) {
+      setText(editor, unescapedJson);
+    }
+  });
+
   context.subscriptions.push(validateJson);
+  context.subscriptions.push(escapeJson);
+  context.subscriptions.push(unescapeJson);
 }
 
 // this method is called when your extension is deactivated
--- src/helper.ts
+++ src/helper.ts
@@ -1,12 +1,68 @@
 import * as JsonBigint from "json-bigint";
 
 class Helper {
+  /**
+   * isValid
+   * @param text
+   */
   public isValid(text: string): boolean {
     try {
       return typeof JsonBigint.parse(text) === "object";
     } catch (e) {
       return false;
     }
+  }
+
+  /**
+   * escape
+   * @param text
+   */
+  public escape(text: string): string {
+    return this.isValid(text)
+      ? JSON.stringify(text).replace(/^"/g, "").replace(/"$/g, "")
+      : text;
+  }
+
+  /**
+   * unescape
+   * @param text
+   */
+  public unescape(text: string): string {
+    let formattedText = text;
+    try {
+      if (!text.startsWith('"')) {
+        formattedText = '"'.concat(formattedText);
+      }
+
+      if (!text.endsWith('"')) {
+        formattedText = formattedText.concat('"');
+      }
+
+      return JSON.parse(formattedText);
+    } catch (err) {
+      return text;
+    }
+  }
+
+  /**
+   * beautify
+   * @param text
+   * @param tabSize
+   */
+  public beautify(text: string, tabSize?: number | string): string {
+    return this.isValid(text)
+      ? JSON.stringify(JSON.parse(text), null, tabSize)
+      : text;
+  }
+
+  /**
+   * uglify
+   * @param text
+   */
+  public uglify(text: string): string {
+    return this.isValid(text)
+      ? JSON.stringify(JSON.parse(text), null, 0)
+      : text;
   }
 }
 
